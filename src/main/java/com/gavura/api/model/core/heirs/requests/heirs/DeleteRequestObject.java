51ccdb22e271081eb24493f3ba4be3bb68cecd1b
Kostiantyn Gavura <gavura@duck.com>
2022-04-02 10:33:23 +0200

    add DeleteRequestObject

--- src/main/java/com/gavura/api/model/core/heirs/requests/heirs/DeleteRequestObject.java
+++ src/main/java/com/gavura/api/model/core/heirs/requests/heirs/DeleteRequestObject.java
@@ -3,14 +3,25 @@ package com.gavura.api.model.core.heirs.requests.heirs;
 import com.gavura.api.model.core.heirs.requests.RqObject;
 
 public class DeleteRequestObject extends RqObject {
+
+    public static final String FORMAT_PATTER_FOR_URI = "%s%s%s";
+
     public DeleteRequestObject(String rqName) {
         super(rqName);
     }
 
-    public void createRequestForDeletingUser(String userName) {
-        setBaseUri(String.format("%s%s%s",BASE_URI, "/user/", userName));
+    public void createRequestForDeletingUserBy(String userName) {
+        setBaseUri(String.format(FORMAT_PATTER_FOR_URI, BASE_URI, "/user/", userName));
         setCommonParams();
     }
 
+    public void createRequestForDeletingPetBy(int petId) {
+        setBaseUri(String.format(FORMAT_PATTER_FOR_URI, BASE_URI, "/pet/", petId));
+        setCommonParams();
+    }
 
+    public void createRequestForDeletingPurchaseOrderBy(int orderId) {
+        setBaseUri(String.format(FORMAT_PATTER_FOR_URI, BASE_URI, "/store/order/", orderId));
+        setCommonParams();
+    }
 }
